d586e8b10003dbccfce12b5cf2dae2f45994e5a5
Askydev <43278273+Askydev@users.noreply.github.com>
2019-05-29 21:29:52 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+  $(document).ready(function(){
+    $('.carousel').carousel();
+    $(".button-collapse").sideNav();
+    $('.scrollspy').scrollSpy();
+    $('.parallax').parallax();
+    });
+
+
+
+
